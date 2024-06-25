0099bada0bb358b4bad0ba9314b6be9ccb5e3d22
Denys Daneliuk <d.danelyuk@gmail.com>
2024-06-25 10:54:44 +0300

    message & conversation models

---
+++ server/models/conversation.model.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const conversationSchema = new mongoose.Schema({
+  particioants: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ],
+  messages: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Message',
+      default: []
+    }
+  ],
+}, { timestamps: true });
+
+const Conversation = mongoose.model("Conversations", conversationSchema);
+
+export default Conversation;
