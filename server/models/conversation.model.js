1ccdd5e5d56042b3399a755e30455f20e18e8752
Denys Daneliuk <d.danelyuk@gmail.com>
2024-06-26 22:13:54 +0300

    fix field name in model

--- server/models/conversation.model.js
+++ server/models/conversation.model.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const conversationSchema = new mongoose.Schema({
-  particioants: [
+  participants: [
     {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User'
