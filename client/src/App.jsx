1802c47ac958b70f9e8b2207b1b19fc98b98aac0
Denys Daneliuk <d.danelyuk@gmail.com>
2024-06-29 23:43:50 +0300

    ui add message temaplate

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,6 @@ import "./App.css";
 function App() {
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-     <Login />
      <Home />
     </div>
   )
--- client/src/components/sidebar/LogoutBtn.jsx
+++ client/src/components/sidebar/LogoutBtn.jsx
@@ -1,16 +1,12 @@
 import { BiLogOut } from "react-icons/bi";
-//import useLogout from "../../hooks/useLogout";
+// import useLogout from "../../hooks/useLogout";
 
 const LogoutBtn = () => {
-	//const { loading, logout } = useLogout();
+	// const { loading, logout } = useLogout();
 
 	return (
 		<div className='mt-auto'>
-			{/* {!loading ? (
-				<BiLogOut className='w-6 h-6 text-white cursor-pointer' onClick={logout} />
-			) : (
-				<span className='loading loading-spinner'></span>
-			)} */}
+				<BiLogOut className='w-6 h-6 text-white cursor-pointer' />
 		</div>
 	);
 };
