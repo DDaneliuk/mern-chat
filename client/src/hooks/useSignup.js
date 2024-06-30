36dcfb6df5116fc40e9803738f763a46a6de2da7
Denys Daneliuk <d.danelyuk@gmail.com>
2024-06-30 22:57:09 +0300

    add useSignup hook

---
+++ client/src/hooks/useSignup.js
@@ -0,0 +1,63 @@
+import { stringify } from 'postcss';
+import { useState } from 'react';
+import toast from 'react-hot-toast';
+import { useAuthContext } from '../context/AuthContext';
+
+const useSignup = () => {
+  const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext();
+ 
+  const signup = async (inputs) => {
+    const success = handleInputErrors(inputs);
+    if(!success) return;
+
+    setLoading(true);
+
+    try{
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(inputs)
+      })
+
+      const data = await res.json();
+      if(data.error){
+        throw new Error(data.error);
+      }
+
+      console.log('data', data);
+      localStorage.setItem('user', JSON.stringify(data));
+      setAuthUser(data);
+
+    } catch (e) {
+      console.log(e.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return { loading, signup };
+}
+
+export default useSignup
+
+function handleInputErrors(inputs){
+  const { name, username, password, confirmPassword } = inputs;
+  console.log(!!name, !!username, !!password, !!confirmPassword)
+  if(!name || !username || !password || !confirmPassword){
+    toast.error('Please fill in all fields');
+    return false
+  }
+
+  if(password !== confirmPassword) {
+    toast.error('Passwords dont match');
+    return false
+  }
+
+  if(password.length < 6) {
+    toast.error('Passwords must be at least 6 characters');
+    return false
+  }
+
+  return true
+};
