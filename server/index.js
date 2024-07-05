d1a32e89c3f706ce514abcfa83417c6d387fc6aa
Denys Daneliuk <d.danelyuk@gmail.com>
2024-07-06 01:08:11 +0300

    build

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
-import { app, server } from "./socket/socket.js";
+import path from "path";
 import express from "express";
+import { app, server } from "./socket/socket.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 // db
@@ -11,6 +12,8 @@ import messageRoutes from "./routes/message.routes.js";
 
 const PORT = process.env.PORT || 3000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json());
@@ -19,6 +22,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
+})
 
 server.listen(PORT, () => {
   connectMongo();
