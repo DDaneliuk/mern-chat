2293c13a6b5e9859b0ba1ad3bbd31cf12c2e9d60
Denys Daneliuk <d.danelyuk@gmail.com>
2024-07-05 18:47:20 +0300

    update UI

--- client/src/components/input/SearchInput.jsx
+++ client/src/components/input/SearchInput.jsx
@@ -1,9 +1,17 @@
 import { IoSearchSharp } from "react-icons/io5";
 
 const SearchInput = () => {
+	const [search, setSearch] = useState('');
+
+	const handleSubmit = () => {
+		e.preventDefault();
+	}
+
 	return (
-		<form className='flex items-center gap-2'>
-			<input type='text' placeholder='Search…' className='input input-bordered rounded-full' />
+		<form onSubmit={handleSubmit} className='flex items-center gap-2'>
+			<input type='text' placeholder='Search…' className='input input-bordered rounded-full'
+				value={search}
+				onChange={(e) => setSearch(e.target.value)} />
 			<button type='submit' className='btn btn-circle bg-sky-500 text-white'>
 				<IoSearchSharp className='w-6 h-6 outline-none' />
 			</button>
--- client/src/components/messages/Messages.jsx
+++ client/src/components/messages/Messages.jsx
@@ -27,7 +27,10 @@ const Messages = () => {
 			{loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
 
 			{!loading && messages.length === 0 && (
-				<p className='text-center'> Send a message to start the conversation</p>
+				<div className='px-4 flex-1 flex overflow-auto justify-center items-center h-full flex-col'>
+					<h2 className="text-2xl text-white">Say Hello!</h2>
+					<p className='text-center'>Send a message to start the conversation</p>
+				</div>
 			)}
 		</div>
 	);
